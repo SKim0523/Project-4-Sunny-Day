c4aeffc9a650ba4c02e72de39d62f7efab054d87
SKim0523 <emailsarahkim@gmail.com>
2022-06-13 02:05:17 -0400

    heroku update

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
-
 from main_app.forms import ScheduleCreateForm
 from .models import Day, Schedule
 from django.views.generic.edit import UpdateView, DeleteView
@@ -13,7 +12,6 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-
 
 @method_decorator(login_required, name='dispatch')
 class DayCreate(View):
@@ -29,38 +27,28 @@ class DayCreate(View):
         Schedule.objects.create(time=time, content=content, day_id=dayContent.id)
         return redirect('daily_schedule')
 
-
 @method_decorator(login_required, name='dispatch')
 class ScheduleCreate(View):
-    # Creating the form
-    def get(self, request, day_id, *args, **kwargs):
-        # This comes from forms.py
+    def get(self, request, day_id):
         form = ScheduleCreateForm()
-        # Give the form to the template
         return render(request, "schedule_create.html", context={'form':form})
-    # Post funstion runs when we submit the form
     def post(self, request, day_id):
-        # request.POST = form data that was submitted through this request
         form = ScheduleCreateForm(request.POST)
         if form.is_valid():
-            # Will create and save a new schedule object
-            #commit = False >> Don't save yet because we don't have all the information yet
             schedule = form.save(commit=False)
             schedule.user = request.user
             schedule.day_id = day_id
             schedule.save()
             return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
-        return render(request, "schedule_create2.html", context={'form':form})
+        return render(request, "schedule_create.html", context={'form':form})
     
-
 @method_decorator(login_required, name='dispatch')
 class ScheduleUpdate(UpdateView):
     model = Schedule
     fields = ['time', 'content', 'day']
     template_name = "schedule_update.html"
     success_url = "/daily/"
-    
-    
+      
 @method_decorator(login_required, name='dispatch')    
 class MemoUpdate(UpdateView):
     model = Day
@@ -68,13 +56,11 @@ class MemoUpdate(UpdateView):
     template_name = "memo_update.html"
     success_url = "/daily/"   
 
-
 @method_decorator(login_required, name='dispatch')
 class ScheduleDelete(DeleteView):
     model = Schedule
     template_name = "schedule_delete.html"
     success_url = "/daily/"
-
 
 @method_decorator(login_required, name='dispatch')
 class DailySchedule(TemplateView):
@@ -85,7 +71,6 @@ class DailySchedule(TemplateView):
         context = super().get_context_data(**kwargs)
         context["days"] = Day.objects.filter(date__in=[today, tomorrow]) 
         return context
-
 
 @method_decorator(login_required, name='dispatch')
 class WeeklySchedule(TemplateView):
@@ -101,28 +86,22 @@ class WeeklySchedule(TemplateView):
         context["week_day"] = day_of_week
         context["days"] = Day.objects.filter(date__range=[week_start, week_end]) 
         return context
-    
-# https://java2blog.com/last-day-of-month-python/    
+     
 @method_decorator(login_required, name='dispatch')
-class MonthlySchedule(TemplateView):
-    template_name = "monthly_view.html"
+class MonthlySchedule(View):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get(self, request):
+        context = {}
         date = datetime.now().date()
         current_month_name = calendar.month_name[date.today().month]
         month_start = date.replace(day = 1)
         month_end = date.replace(day = calendar.monthrange(date.year, date.month)[1])
-        # print(date)
-        # print(month_start)
-        # print(month_end)
         context["current_month"] = current_month_name
         context["first_day"] = month_start
         context["last_day"] = month_end
+        context["range"] = range(month_start.day, month_end.day+1)
         context["days"] = Day.objects.filter(date__range=[month_start, month_end]) 
-        return context
-    # def dateClick(self):  >> click a date and display relevant schedule? Is this possible?
-    
+        return render(request, "monthly_view.html", context=context)
     
 class Signup(View):
     def get(self, request):
